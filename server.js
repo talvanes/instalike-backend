19e22077739c76db0e18925be32723cce8c98bc2
Talvanes Ferreira de Sousa <contato@talvanes.dev>
2024-11-19 15:44:09 -0300

    Alura Back-End: Day 2

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    description: "Millie the tabby kitty",
+    image: "https://placecats.com/millie/300/150"
+  },
+  {
+    id: 2,
+    description: "Sunset over the ocean",
+    image: "https://placecats.com/beach/500/250"
+  },
+  {
+    id: 3,
+    description: "Cute kitten",
+    image: "https://placecats.com/kitten/200/100"
+  },
+  {
+    id: 4,
+    description: "Mountain landscape",
+    image: "https://placecats.com/mountains/600/300"
+  }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
-    console.log("Listening on port 3000...");
+  console.log("Listening on port 3000...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("The Eiffel Tower, lit up at night with thousands of twinkling lights, makes a magical spectacle in Paris.");
+function fetchPostByID(id) {
+  return posts.findIndex((post) => post.id === Number(id));
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = fetchPostByID(req.params.id);
+  res.status(200).json(posts[index]);
 });
